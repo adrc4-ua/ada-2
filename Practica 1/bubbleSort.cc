7a4c521e62520c15461bbfd19c03befb7f031e64
Adrián Ruiz <adrc4@alu.ua.es>
2026-02-02 14:42:04 +0100

    Avance practica 1

--- Practica 1/bubbleSort.cc
+++ Practica 1/bubbleSort.cc
@@ -65,5 +65,3 @@ int main(void){
         delete[] v; 
     }
 }
-
-
--- Practica 1/quickSort.cc
+++ Practica 1/quickSort.cc
@@ -3,29 +3,91 @@ ADA 2025-26
 Práctica 1: "Empirical analysis of Quicksort average-time complexity."
 */
 
+#include <unistd.h>
+#include <iostream>
+#include <math.h>
+#include <time.h>
+
+using namespace std;
+
 //--------------------------------------------------------------
 // Middle QuickSort
-void 
-middle_QuickSort(int * v, long left, long right){
 
-    long i,j;
-    int pivot,aux; 
-    if (left<right)
+void middle_QuickSort(int *v, long left, long right)
+{
+
+    long i, j;
+    int pivot, aux;
+    if (left < right)
     {
-        i=left; j=right;
-        pivot=v[(i+j)/2];
+        i = left;
+        j = right;
+        pivot = v[(i + j) / 2];
         do
         {
-            while (v[i]<pivot) i++;
-            while (v[j]>pivot) j--;
-            if (i<=j)
+            while (v[i] < pivot)
+                i++;
+            while (v[j] > pivot)
+                j--;
+            if (i <= j)
             {
-                aux=v[i]; v[i]=v[j]; v[j]=aux;
-                i++; j--;
+                aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+                i++;
+                j--;
             }
-       } while (i<=j);
-       if (left<j)  middle_QuickSort(v,left,j);
-       if (i<right) middle_QuickSort(v,i,right);
+        } while (i <= j);
+        if (left < j)
+            middle_QuickSort(v, left, j);
+        if (i < right)
+            middle_QuickSort(v, i, right);
     }
 }
 
+int main(void)
+{
+
+    srand(time(NULL));
+
+    cout << "# QuickSort CPU-times in milliseconds:"
+         << endl
+         << "# Size \t Average CPU time (ms.)"
+         << endl
+         << "# ----------------------------"
+         << endl;
+
+    for (int exp = 15; exp <= 20; exp++)
+    {
+        size_t size = size_t(pow(2, exp));
+        int *v = new int[size];
+        if (!v)
+        {
+            cerr << "Error, not enough memory!" << endl;
+            exit(EXIT_FAILURE);
+        }
+
+        cout << size << "\t\t" << std::flush;
+
+        auto start = 0;
+        auto end = 0;
+        for (int i = 0; i <= 30; i++)
+        {
+            for (size_t j = 0; j < size; j++)
+                v[j] = rand();
+
+            start += clock();
+            middle_QuickSort(v, 0, size - 1);
+            end += clock();
+
+            for (size_t i = 1; i < size; i++)
+                if (v[i] < v[i - 1])
+                {
+                    cerr << "Panic, array not sorted! " << i << endl;
+                    exit(EXIT_FAILURE);
+                }
+        }
+        cout << 1000.0 * (end / 30 - start / 30) / CLOCKS_PER_SEC << endl;
+        delete[] v;
+    }
+}
